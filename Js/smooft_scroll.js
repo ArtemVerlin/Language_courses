74514cb1da4f150cc1a2b08f6a92c545d5919b2b
Andrey Dunaev <130069260+Xoolm@users.noreply.github.com>
2023-04-18 11:05:21 +0300

    Master (#2)
    1. Added a form field validator to ensure that all required fields are filled in correctly.
    
    2. Implemented modal windows for the "book a call" and "thank you" pages, as well as a calendar popup window for date selection.
    
    3. Temporarily connected the calendar library Jquery UI to the project to facilitate the popup window functionality.
    
    4. Fixed initial bugs to improve the overall stability and functionality of the landing page.
    
    5 . Made design improvements to enhance the visual appeal and user experience.
    
    6. Converted px to rem for improved adaptability across different screen sizes.
    
    7. Added page navigation functionality to the landing page to facilitate easy access to different sections of the website.
    
    ---------
    
    Co-authored-by: ArtemVerlin <verlin.artem@gmail.com>

---
+++ Js/smooft_scroll.js
@@ -0,0 +1,29 @@
+/************************************Плавная прокрутка******************************************/
+const menuLinks = document.querySelectorAll('.first_section__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockBValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('header').offsetHeight;
+
+			// if (iconMenu.classList.contains('_active')) {
+			// 	document.body.classList.remove('_lock');
+			// 	iconMenu.classList.remove('_active');
+			// 	headerNav.classList.remove('_active');
+			// }
+
+			window.scrollTo({
+				top: gotoBlockBValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
+/************************************Плавная прокрутка******************************************/
