bee82e278ed36090fe01df925a12ada13acd77a5
Andrey Dunaev <andreydunaev5@gmail.com>
2023-04-21 20:35:32 +0300

    menu burger bug fixes

--- Js/menu-burger.js
+++ Js/menu-burger.js
@@ -2,7 +2,7 @@ const iconMenu = document.querySelector('.menu__icon');
 const headerNav = document.querySelector('.header__nav');
 if(iconMenu) {
 	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('_lock');
+		// document.body.classList.toggle('_lock');
 		iconMenu.classList.toggle('_active');
 		headerNav.classList.toggle('_active');
 	});
